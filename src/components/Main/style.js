3613b0c84e5c5bc9b0e263a0c006860e2128f079
Lelouching <engenheirogamer123@gmail.com>
2022-11-25 18:01:35 -0300

    fix: container não estava sendo aplicado corretamente na tag main

--- src/components/Main/style.js
+++ src/components/Main/style.js
@@ -1,13 +1,16 @@
 import styled from "styled-components"
 
 export const MainStyle = styled.main`
-    width: 100%;
+    width: 90%;
+    margin: 0 auto;
     height: fit-content;
     display: flex;
     flex-direction: column;
     gap: 3rem;
 
     @media (min-width:701px) {
+        width: 1200px;
+        max-width: 100%;
         flex-direction: row;
         justify-content: space-between;
         gap: 1.5rem;
